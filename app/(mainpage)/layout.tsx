c570dc659248d0d3f69f239d13e684f4d6055ebd
Zhiliu Sara Ho <z28he@uwaterloo.ca>
2025-02-12 15:08:32 -0500

    feat: add sidebar and customize logo

--- app/(mainpage)/layout.tsx
+++ app/(mainpage)/layout.tsx
@@ -1,7 +1,14 @@
-export default function HomeLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return <div>{children}</div>;
+import Sidebar from "../../components/sidebar";
+// default export so no need to destructure
+function HomeLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <div className="h-full">
+      <div className="hidden md:flex h-full w-56 flex-col fixed inset-y-0 z-50">
+        <Sidebar />
+      </div>
+      <main className="md:pl-56 pt-[80px] h-full">{children}</main>
+    </div>
+  );
 }
+
+export default HomeLayout;

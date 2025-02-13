4f08e85e1c9d0e547af2828bac5c79a98ece9863
Zhiliu Sara Ho <z28he@uwaterloo.ca>
2025-02-12 22:00:12 -0500

    feat and style: sidebar in the mobile screen size

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,5 +1,11 @@
+import MobileSidebar from "./mobile-sidebar";
+
 function Navbar() {
-  return <nav>this is a navbar</nav>;
+  return (
+    <nav className="p-4 border-b h-full flex items-center bg-white shadow-sm">
+      <MobileSidebar />
+    </nav>
+  );
 }
 
 export default Navbar;

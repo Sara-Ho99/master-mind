0ee8972e68d0201cc5eabe09025bb030f8016dc4
Zhiliu Sara Ho <z28he@uwaterloo.ca>
2025-02-12 22:42:26 -0500

    feat: add basic structure and style for creator mode button on navbar

--- components/navbar-routes.tsx
+++ components/navbar-routes.tsx
@@ -1,9 +1,31 @@
 "use client";
 import { UserButton } from "@clerk/nextjs";
+import { usePathname, useRouter } from "next/navigation";
+import { Button } from "@/components/ui/button";
+import { LogOut } from "lucide-react";
+import Link from "next/link";
 
 function NavbarRoutes() {
+  const pathname = usePathname();
+  const router = useRouter();
+
+  const isCreatorPage = pathname?.startsWith("/creator");
+  const isCoursePage = pathname?.includes("/courses");
+
   return (
     <div className="flex gap-x-2 ml-auto">
+      {isCreatorPage || isCoursePage ? (
+        <Button size="sm" variant="ghost">
+          <LogOut className="h-4 w-4 mr-2" />
+          Exit
+        </Button>
+      ) : (
+        <Link href="/creator/courses">
+          <Button size="sm" variant="custom">
+            creator mode
+          </Button>
+        </Link>
+      )}
       <UserButton />
     </div>
   );

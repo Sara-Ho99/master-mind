2b0af9c991b89d28b01923e522523df8b5a697e9
Zhiliu Sara Ho <z28he@uwaterloo.ca>
2025-02-12 23:01:10 -0500

    feat: add navigation from creator mode to learner mode

--- components/navbar-routes.tsx
+++ components/navbar-routes.tsx
@@ -1,13 +1,12 @@
 "use client";
 import { UserButton } from "@clerk/nextjs";
-import { usePathname, useRouter } from "next/navigation";
+import { usePathname } from "next/navigation";
 import { Button } from "@/components/ui/button";
-import { LogOut } from "lucide-react";
+import { LogIn, LogOut } from "lucide-react";
 import Link from "next/link";
 
 function NavbarRoutes() {
   const pathname = usePathname();
-  const router = useRouter();
 
   const isCreatorPage = pathname?.startsWith("/creator");
   const isCoursePage = pathname?.includes("/courses");
@@ -15,14 +14,17 @@ function NavbarRoutes() {
   return (
     <div className="flex gap-x-2 ml-auto">
       {isCreatorPage || isCoursePage ? (
-        <Button size="sm" variant="custom">
-          <LogOut className="h-4 w-4 mr-2" />
-          Exit
-        </Button>
+        <Link href="/">
+          <Button size="sm" variant="custom">
+            <LogOut className="h-4 w-4 mr-2" />
+            Learner mode
+          </Button>
+        </Link>
       ) : (
         <Link href="/creator">
           <Button size="sm" variant="custom">
-            creator mode
+            <LogIn className="h-4 w-4 mr-2" />
+            Creator mode
           </Button>
         </Link>
       )}

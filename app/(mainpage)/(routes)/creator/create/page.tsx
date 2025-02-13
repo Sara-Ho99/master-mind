b24e5c22c2adb41b72bed3aaf447891cfd0804e8
Zhiliu Sara Ho <z28he@uwaterloo.ca>
2025-02-13 00:05:13 -0500

    feat: use zod for form validation

--- app/(mainpage)/(routes)/creator/create/page.tsx
+++ app/(mainpage)/(routes)/creator/create/page.tsx
@@ -1,6 +1,32 @@
 "use client";
+import * as z from "zod"; // zod for schema validation
+import axios from "axios";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { useRouter } from "next/navigation";
+import Link from "next/link";
+
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormLabel,
+  FormMessage,
+  FormItem,
+} from "@/components/ui/form";
+
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 
 function CreatePage() {
+  // Form validation
+  const formSchema = z.object({
+    title: z.string().min(1, {
+      message: "Title is required",
+    }),
+  });
+
   return (
     <div>
       <h1>CreatePage</h1>

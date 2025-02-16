8eb3a54cd60fe84b1ece048ae71b3d60aacfded6
Zhiliu Sara Ho <z28he@uwaterloo.ca>
2025-02-16 12:01:18 -0500

    feat: course title edit and cancel button

--- components/title-form.tsx
+++ components/title-form.tsx
@@ -1,9 +1,67 @@
 "use client";
 
-function TitleForm() {
+import * as z from "zod";
+import axios from "axios";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import toast from "react-hot-toast";
+import { Pencil } from "lucide-react";
+import { useState } from "react";
+
+interface TitleFormProps {
+  initialData: {
+    title: string;
+  };
+  courseId: string;
+}
+
+const formSchema = z.object({
+  title: z.string().min(1, {
+    message: "Title is required",
+  }),
+});
+
+function TitleForm({ initialData, courseId }: TitleFormProps) {
+  const [isEditing, setIsEditing] = useState(false);
+  const toggleEdit = () => setIsEditing((prev) => !prev);
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: initialData,
+  });
+  const { isSubmitting, isValid } = form.formState;
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    try {
+      console.log(values);
+      toast.success("Title updated");
+    } catch {
+      toast.error("Something went wrong");
+    }
+  };
   return (
-    <div>
-      <h1>Title Form</h1>
+    <div className="mt-6 border bg-slate-100 rounded-md p-4">
+      <div className="font-medium flex items-center justify-between">
+        Course Title
+        <Button onClick={toggleEdit} variant="ghost">
+          {isEditing ? (
+            <>Cancel</>
+          ) : (
+            <>
+              <Pencil className="h-4 w-4 mr-0" />
+              Edit title
+            </>
+          )}
+        </Button>
+      </div>
     </div>
   );
 }

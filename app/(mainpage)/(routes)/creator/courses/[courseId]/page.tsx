814d35f5d3803d7ec3031732c994da9935d48529
Zhiliu Sara Ho <z28he@uwaterloo.ca>
2025-02-16 11:06:36 -0500

    feat: add course setup title, fetch data from db

--- app/(mainpage)/(routes)/creator/courses/[courseId]/page.tsx
+++ app/(mainpage)/(routes)/creator/courses/[courseId]/page.tsx
@@ -1,9 +1,39 @@
-function CourseIdPage() {
+import { redirect } from "next/navigation";
+import { db } from "@/lib/db";
+
+const CourseIdPage = async ({ params }: { params: { courseId: string } }) => {
+  const course = await db.course.findUnique({
+    where: {
+      id: params.courseId,
+    },
+  });
+  if (!course) {
+    return redirect("/");
+  }
+
+  const requiredFields = [
+    course.title,
+    course.description,
+    course.price,
+    course.categoryId,
+  ];
+
+  const totalFields = requiredFields.length;
+  const completedFields = requiredFields.filter(Boolean).length;
+
+  const completionText = `(${completedFields}/${totalFields})`;
   return (
-    <div>
-      <h1>CourseIdPage</h1>
+    <div className="p-6">
+      <div className="flex items-center justify-between">
+        <div className="flex flex-col gap-y-2">
+          <h1 className="text-2xl font-medium">Course setup</h1>
+          <span className="text-sm text-slate-700">
+            Complete all fields {completionText}
+          </span>
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default CourseIdPage;

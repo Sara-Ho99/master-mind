0c0ae9735ef28e8bf101433f580a48953d4deeee
Zhiliu Sara Ho <z28he@uwaterloo.ca>
2025-02-16 22:59:31 -0500

    add category data and push to neon db

--- app/(mainpage)/(routes)/creator/courses/[courseId]/page.tsx
+++ app/(mainpage)/(routes)/creator/courses/[courseId]/page.tsx
@@ -15,6 +15,12 @@ const CourseIdPage = async ({ params }: { params: { courseId: string } }) => {
     return redirect("/");
   }
 
+  const categories = await db.category.findMany({
+    orderBy: {
+      name: "asc",
+    },
+  });
+
   const requiredFields = [
     course.title,
     course.description,

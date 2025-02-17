1e4d8c580c4447b5828bc74387acfc42c00ddc78
Zhiliu Sara Ho <z28he@uwaterloo.ca>
2025-02-17 17:14:18 -0500

    feat: create course chapters (title) in course edit page, chapter drag-reorder function

--- components/chapters-form.tsx
+++ components/chapters-form.tsx
@@ -14,12 +14,13 @@ import {
 
 import { Button } from "@/components/ui/button";
 import toast from "react-hot-toast";
-import { PlusSquare } from "lucide-react";
+import { PlusSquare, Loader } from "lucide-react";
 import { useState } from "react";
 import { useRouter } from "next/navigation";
 import { Input } from "./ui/input";
 import { cn } from "@/lib/utils";
 import { Course, Chapter } from "@prisma/client";
+import ChaptersList from "@/components/chapters-list";
 
 interface ChaptersFormProps {
   initialData: Course & { chapters: Chapter[] };
@@ -36,6 +37,7 @@ function ChaptersForm({ initialData, courseId }: ChaptersFormProps) {
   const [isUpdating, setIsUpdating] = useState(false);
 
   const toggleCreate = () => setIsCreating((prev) => !prev);
+
   const router = useRouter();
 
   const form = useForm<z.infer<typeof formSchema>>({
@@ -50,16 +52,40 @@ function ChaptersForm({ initialData, courseId }: ChaptersFormProps) {
   const onSubmit = async (values: z.infer<typeof formSchema>) => {
     try {
       await axios.post(`/api/courses/${courseId}/chapters`, values);
-      toast.success("Chapter title created");
+      toast.success("Chapter created");
       toggleCreate();
       router.refresh();
     } catch {
       toast.error("Something went wrong");
     }
   };
+  const onReorder = async (updateData: { id: string; position: number }[]) => {
+    try {
+      setIsUpdating(true);
+
+      await axios.put(`/api/courses/${courseId}/chapters/reorder`, {
+        list: updateData,
+      });
+      toast.success("Chapters reordered");
+      router.refresh();
+    } catch {
+      toast.error("Something went wrong");
+    } finally {
+      setIsUpdating(false);
+    }
+  };
+
+  const onEdit = (id: string) => {
+    router.push(`/teacher/courses/${courseId}/chapters/${id}`);
+  };
 
   return (
-    <div className="mt-6 border bg-sky-200/20 rounded-md p-4">
+    <div className="relative mt-6 border bg-sky-200/20 rounded-md p-4">
+      {isUpdating && (
+        <div className="absolute h-full w-full bg-slate-500/20 top-0 right-0 rounded-m flex items-center justify-center">
+          <Loader className="animate-spin h-6 w-6 text-sky-700" />
+        </div>
+      )}
       <div className="font-medium flex items-center justify-between">
         Course Chapters
         <Button onClick={toggleCreate} variant="custom">
@@ -115,11 +141,11 @@ function ChaptersForm({ initialData, courseId }: ChaptersFormProps) {
           )}
         >
           {!initialData.chapters.length && "No chapters"}
-          {/* <ChaptersList
-            onEdit={onEdit}
+          <ChaptersList
+            onEdit={() => {}}
             onReorder={onReorder}
             items={initialData.chapters || []}
-          /> */}
+          />
         </div>
       )}
       {!isCreating && initialData.chapters.length > 1 && (

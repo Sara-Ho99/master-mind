a91f0005514e31c39e9eb2148d40a5a4cbf3c073
Zhiliu Sara Ho <z28he@uwaterloo.ca>
2025-02-17 21:36:20 -0500

    feat: basic structure for chapter create page

--- app/(mainpage)/(routes)/creator/courses/[courseId]/chapters/[chapterId]/page.tsx
+++ app/(mainpage)/(routes)/creator/courses/[courseId]/chapters/[chapterId]/page.tsx
@@ -1,9 +1,89 @@
-function ChapterIdPage() {
+import { redirect } from "next/navigation";
+import Link from "next/link";
+import { ArrowLeft, PlusCircle, Video, ShieldCheck } from "lucide-react";
+
+import { db } from "@/lib/db";
+
+const ChapterIdPage = async ({
+  params,
+}: {
+  params: { courseId: string; chapterId: string };
+}) => {
+  const chapter = await db.chapter.findUnique({
+    where: {
+      id: params.chapterId,
+      courseId: params.courseId,
+    },
+    include: {
+      muxData: true,
+    },
+  });
+
+  if (!chapter) {
+    return redirect("/");
+  }
+
+  const requiredFields = [chapter.title, chapter.description, chapter.videoUrl];
+
+  const totalFields = requiredFields.length;
+  const completedFields = requiredFields.filter(Boolean).length;
+
+  const completionText = `(${completedFields}/${totalFields})`;
+
+  const isComplete = requiredFields.every(Boolean);
+
   return (
-    <div>
-      <h1>ChapterId Page</h1>
-    </div>
+    <>
+      <div className="p-6">
+        <div className="flex items-center justify-between">
+          <div className="w-full">
+            <Link
+              href={`/creator/courses/${params.courseId}`}
+              className="flex items-center text-sm hover:opacity-75 transition mb-6"
+            >
+              <ArrowLeft className="h-4 w-4 mr-2" />
+              Back to Course Setup
+            </Link>
+            <div className="flex items-center justify-between w-full">
+              <div className="flex flex-col gap-y-2">
+                <h1 className="text-2xl font-medium">Chapter Creation</h1>
+                <span className="text-sm text-slate-700">
+                  Complete all fields {completionText}
+                </span>
+              </div>
+              <div>ACTION</div>
+            </div>
+          </div>
+        </div>
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mt-16">
+          <div className="space-y-4">
+            <div>
+              <div className="flex items-center gap-x-2">
+                <PlusCircle size="30" />
+                <h2 className="text-xl">Customize your chapter</h2>
+              </div>
+              <div>Title form</div>
+              <div>Description form</div>
+            </div>
+            <div>
+              <div className="flex items-center gap-x-2">
+                <ShieldCheck size="30" />
+                <h2 className="text-xl">Access Settings</h2>
+              </div>
+              <div>Access form</div>
+            </div>
+          </div>
+          <div>
+            <div className="flex items-center gap-x-2">
+              <Video size="30" />
+              <h2 className="text-xl">Add a video</h2>
+            </div>
+            <div>Video form</div>
+          </div>
+        </div>
+      </div>
+    </>
   );
-}
+};
 
 export default ChapterIdPage;

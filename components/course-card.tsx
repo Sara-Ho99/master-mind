ff9038f3944fd03cc0e3d427628fde23d5382b93
Zhiliu Sara Ho <z28he@uwaterloo.ca>
2025-02-19 16:47:43 -0500

    feat: link course card to course detail page

--- components/course-card.tsx
+++ components/course-card.tsx
@@ -27,25 +27,29 @@ export const CourseCard = ({
   category,
 }: CourseCardProps) => {
   return (
-    <div className="group hover:border-[#8ecae6] hover:shadow-md transition overflow-hidden border rounded-lg p-2 h-full">
-      <div className="relative w-full aspect-video rounded-md overflow-hidden">
-        <Image fill className="object-cover" alt={title} src={imageUrl} />
-      </div>
-      <div className="flex flex-col pt-2">
-        <div className="text-lg md:text-base font-medium line-clamp-2">
-          {title}
+    <Link href={`/courses/${id}`}>
+      <div className="group hover:border-[#8ecae6] hover:shadow-md transition overflow-hidden border rounded-lg p-2 h-full">
+        <div className="relative w-full aspect-video rounded-md overflow-hidden">
+          <Image fill className="object-cover" alt={title} src={imageUrl} />
         </div>
-        <p className="text-xs text-muted-foreground">{category}</p>
-        <div className="mt-3 flex items-center justify-between gap-x-2 text-sm md:text-xs">
-          <div className="flex items-center gap-x-1 text-[#219ebc]">
-            <BookOpen size="20" color="#219ebc" />
-            <span>
-              {chaptersLength} {chaptersLength === 1 ? "Chapter" : "Chapters"}
-            </span>
+        <div className="flex flex-col pt-2">
+          <div className="text-lg md:text-base font-medium line-clamp-2">
+            {title}
           </div>
-          <p className="text-md md:text-sm font-medium">{formatPrice(price)}</p>
+          <p className="text-xs text-muted-foreground">{category}</p>
+          <div className="mt-3 flex items-center justify-between gap-x-2 text-sm md:text-xs">
+            <div className="flex items-center gap-x-1 text-[#219ebc]">
+              <BookOpen size="20" color="#219ebc" />
+              <span>
+                {chaptersLength} {chaptersLength === 1 ? "Chapter" : "Chapters"}
+              </span>
+            </div>
+            <p className="text-md md:text-sm font-medium">
+              {formatPrice(price)}
+            </p>
+          </div>
         </div>
       </div>
-    </div>
+    </Link>
   );
 };

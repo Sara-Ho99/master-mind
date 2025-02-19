0627ac5d0c9a2a9ae402ef3748ec403171bbced3
Zhiliu Sara Ho <z28he@uwaterloo.ca>
2025-02-18 20:44:58 -0500

    style: category panel

--- components/category-item.tsx
+++ components/category-item.tsx
@@ -1,5 +1,7 @@
 "use client";
 import { cn } from "@/lib/utils";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
+import qs from "query-string";
 
 interface CategoryItemProps {
   label: string;
@@ -7,11 +9,34 @@ interface CategoryItemProps {
 }
 
 function CategoryItem({ label, value }: CategoryItemProps) {
+  const pathname = usePathname();
+  const router = useRouter();
+  const searchParams = useSearchParams();
+
+  const currentCategoryId = searchParams.get("categoryId");
+
+  const isSelected = currentCategoryId === value;
+
+  const onClick = () => {
+    const url = qs.stringifyUrl(
+      {
+        url: pathname,
+        query: {
+          categoryId: isSelected ? null : value,
+        },
+      },
+      { skipNull: true, skipEmptyString: true }
+    );
+
+    router.push(url);
+  };
+
   return (
     <button
+      onClick={onClick}
       className={cn(
-        "py-2 px-3 text-sm font-[600] text-[#023047] border-2 border-[#e3f2fd] bg-[#e3f2fd] rounded-full flex items-center gap-x-1 hover:border-[#219ebc] transition"
-        // style for active category
+        "py-2 px-3 text-sm font-[600] text-[#023047] border border-[#e3f2fd] bg-[#e3f2fd] rounded-full flex items-center gap-x-1 hover:border-[#023047] transition",
+        isSelected && "border-[#023047] bg-[#023047] text-white"
       )}
       type="button"
     >

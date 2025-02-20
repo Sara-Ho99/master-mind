05d3088b530786b58dcc3efaf1cb7f65592a868d
Zhiliu Sara Ho <z28he@uwaterloo.ca>
2025-02-20 01:09:39 -0500

    feat: add course cards in MyCourse page

--- app/(mainpage)/(routes)/page.tsx
+++ app/(mainpage)/(routes)/page.tsx
@@ -1,7 +1,21 @@
-export default function HomePage() {
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
+import { getMyCourses } from "@/actions/get-mycourse";
+import { CoursesList } from "@/components/courses-list";
+
+async function HomePage() {
+  const { userId } = await auth();
+
+  if (!userId) {
+    return redirect("/");
+  }
+
+  const { completedCourses, coursesInProgress } = await getMyCourses(userId);
+
   return (
-    <div>
-      <h1>Home page</h1>
+    <div className="p-6 space-y-4">
+      <CoursesList items={[...coursesInProgress, ...completedCourses]} />
     </div>
   );
 }
+export default HomePage;

d49fe5d33fd56ed3ada5cd8b81396d08d76145d1
Zhiliu Sara Ho <z28he@uwaterloo.ca>
2025-02-19 20:49:43 -0500

    feat & style: sidebar item

--- components/course-sidebar-item.tsx
+++ components/course-sidebar-item.tsx
@@ -1,5 +1,66 @@
-function CourseSidebarItem() {
-  return <div>item</div>;
+"use client";
+
+import { CheckCircle, Lock, PlayCircle } from "lucide-react";
+import { usePathname, useRouter } from "next/navigation";
+import { cn } from "@/lib/utils";
+
+interface CourseSidebarItemProps {
+  label: string;
+  id: string;
+  isCompleted: boolean;
+  courseId: string;
+  isLocked: boolean;
+}
+
+function CourseSidebarItem({
+  label,
+  id,
+  isCompleted,
+  courseId,
+  isLocked,
+}: CourseSidebarItemProps) {
+  const pathname = usePathname();
+  const router = useRouter();
+
+  const Icon = isLocked ? Lock : isCompleted ? CheckCircle : PlayCircle;
+  const isActive = pathname?.includes(id);
+
+  const onClick = () => {
+    router.push(`/courses/${courseId}/chapters/${id}`);
+  };
+
+  return (
+    <button
+      onClick={onClick}
+      type="button"
+      className={cn(
+        "flex items-center gap-x-2 text-slate-500 text-sm font-[700] pl-6 transition-all hover:text-slate-600 hover:bg-slate-300/20",
+        isActive &&
+          "text-[#023047] bg-[#8ecae6] hover:bg-[#8ecae6] hover:text-[#023047]",
+        isCompleted && "text-emerald-700 hover:text-emerald-700",
+        isCompleted && isActive && "bg-emerald-200/20"
+      )}
+    >
+      <div className="flex items-center gap-x-2 py-4">
+        <Icon
+          size={22}
+          className={cn(
+            "text-slate-500",
+            isActive && "text-[#8ecae6]",
+            isCompleted && "text-emerald-700"
+          )}
+        />
+        {label}
+      </div>
+      <div
+        className={cn(
+          "ml-auto opacity-0 border-2 border-[#023047] h-full transition-all",
+          isActive && "opacity-100",
+          isCompleted && "border-emerald-700"
+        )}
+      />
+    </button>
+  );
 }
 
 export default CourseSidebarItem;
--- components/course-sidebar.tsx
+++ components/course-sidebar.tsx
@@ -29,13 +29,20 @@ async function CourseSidebar({ course, progressCount }: CourseSidebarProps) {
   });
 
   return (
-    <div className="h-full border-r flex flex-col overflow-y-auto shadow-sm">
-      <div className="p-8 flex flex-col border-b">
-        <h1 className="font-semibold">{course.title}</h1>
+    <div className="h-full border-r flex flex-col overflow-y-auto bg-sky-200/20 shadow-sm">
+      <div className="p-6 flex flex-col border-b">
+        <h1 className="font-bold text-[#023047]">{course.title}</h1>
       </div>
       <div className="flex flex-col w-full">
         {course.chapters.map((chapter) => (
-          <CourseSidebarItem />
+          <CourseSidebarItem
+            key={chapter.id}
+            id={chapter.id}
+            label={chapter.title}
+            isCompleted={!!chapter.userProgress?.[0]?.isCompleted}
+            courseId={course.id}
+            isLocked={!chapter.isFree && !purchase}
+          />
         ))}
       </div>
     </div>
